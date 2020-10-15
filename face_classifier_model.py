07853b8da2bcee80ecb8d8c07d7e2d4bd92b2025
simon-acer <simon@ambolt.io>
2020-10-15 17:46:47 +0200

    Putting it all together in a web cam.

--- face_classifier_model.py
+++ face_classifier_model.py
@@ -6,31 +6,9 @@ from PIL import Image
 from utils.transforms import *
 
 
-class Network(nn.Module):
-
-    def __init__(self):
-        super(Network, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 2)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
 def inference(image, model):
-    transforms_fn = transforms.Compose([transforms.Resize((224, 224)),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    transforms_fn = transforms.Compose([transforms.Resize((128, 128)),
+                                        transforms.ToTensor()])
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.eval()
     with torch.no_grad():
